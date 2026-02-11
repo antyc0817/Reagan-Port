1540974df0763dd229abd05ebe4614dfc4bc31eb
Reagan lung <ragquan@Reagans-MacBook-Air.local>
2026-02-11 01:20:37 -0800

    file name change

--- app/projects/whatsup/page.js
+++ app/projects/whatsup/page.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import styles from "./project.module.css";
+import styles from "../_styles/project.module.css";
 
 export const metadata = {
   title: "WhatsUp | Reagan",
